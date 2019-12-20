3b5229b63cbd31969a80912c18f9f88fc6485ef5
Anton Volodin <volodinant@gmail.com>
2019-12-20 22:51:39 +0300

    научился с мантисой и экспонентой управляться

--- format_float.c
+++ format_float.c
@@ -3,17 +3,62 @@
 /*                                                        :::      ::::::::   */
 /*   format_float.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bhugo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: edestiny <edestiny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 12:21:19 by bhugo             #+#    #+#             */
-/*   Updated: 2019/11/18 12:21:27 by bhugo            ###   ########.fr       */
+/*   Updated: 2019/12/20 22:49:21 by edestiny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_print.h"
+// #include "ft_print.h"
+#include <inttypes.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+#define BGD 0x8000000000000000
 
-int		format_float(t_printf *p)
+typedef union 			print_double
 {
-	p->specifier = 'F';
-	return (1);
+	long double 		a;
+	struct 
+	{
+		unsigned long 	mant:64;
+		unsigned int 	exp:15;
+		char 	sign:1;
+	}					sign;
+	
+}						s_print_double;
+
+
+int main()
+{
+    s_print_double t;
+	t.a  = 12342342.23424212;
+	
+	
+	printf("%Lf\n", t.a);
+    printf("%lu\n", t.sign.mant);
+    printf("%u\n", t.sign.exp);
+    printf("%u\n", t.sign.sign);
+
+    long long arr[64] = {0};
+    int j = 64;
+    for (int i = 0; i < 64; i++) 
+    {
+        if (t.sign.mant & BGD){
+            printf("%c", '1');
+            arr[i] = j;
+        }
+        else
+            printf("%c", '0');
+        
+        t.sign.mant <<= 1;
+        j--;
+        
+    }
+    printf("%c", '\n');
+    for (int i = 0; i < 64; i++)
+        printf("%lli\n", arr[i]2);
+        
+	return 0;	
 }
